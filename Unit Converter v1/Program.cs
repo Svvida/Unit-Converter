efeb5e67dd61013148b5283ce042c19a241f2040
Svvida <ksvvida@gmail.com>
2023-05-15 15:41:30 +0200

    Add project files.

---
+++ Unit Converter v1/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Unit_Converter_v1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
